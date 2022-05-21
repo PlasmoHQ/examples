81eb25a04de1d639899c067d80380179343076c7
Louis <6723574+louisgv@users.noreply.github.com>
2022-05-20 20:02:14 -0400

    Use mainline dependency version for now, for dev version, use plasmo

--- with-env-files/index.d.ts
+++ with-env-files/index.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PLASMO_PUBLIC_SHIP_NAME?: string
-    PLASMO_PUBLIC_SHIELD_FREQUENCY?: number
+    PLASMO_PUBLIC_SHIELD_FREQUENCY?: string
   }
 }
---
+++ with-supabase/index.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PLASMO_PUBLIC_SUPABASE_URL?: string
+    PLASMO_PUBLIC_SUPABASE_KEY?: string
+  }
+}
