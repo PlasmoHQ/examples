e73d2127ad23a2e7cee1f2ad2634dc91702ea8e7
Louis <6723574+louisgv@users.noreply.github.com>
2022-05-30 16:02:28 -0400

    Bump dependencies

--- with-popup/popup.tsx
+++ with-popup/popup.tsx
@@ -10,10 +10,10 @@ function IndexPopup() {
         flexDirection: "column",
         padding: 16
       }}>
-        <h1>
-          Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-        </h1>
-        <input onChange={(e)=> setData(e.target.value)} value={data}/>
+      <h1>
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+      </h1>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
     </div>
   )
 }
