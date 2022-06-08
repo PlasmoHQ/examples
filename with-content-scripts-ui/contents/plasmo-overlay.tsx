2a4eac229c9e3b20e2c2f4b6e0d0cbe0296ef843
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-08 02:58:29 -0400

    Add inject style to content script ui example

--- with-content-scripts-ui/contents/plasmo-overlay.tsx
+++ with-content-scripts-ui/contents/plasmo-overlay.tsx
@@ -1,3 +1,4 @@
+import cssText from "bundle-text:./plasmo-overlay.css"
 import type { PlasmoContentScript } from "plasmo"
 
 export const config: PlasmoContentScript = {
@@ -8,9 +9,16 @@ export const config: PlasmoContentScript = {
 // Idea for static mount
 // Idea for styling injection support (inline or with custom emotion cache)
 
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
+
 const PlasmoOverlay = () => {
   return (
     <span
+      className="hw-top"
       style={{
         background: "white",
         padding: 12
