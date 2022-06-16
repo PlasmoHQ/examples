59cfa6850ec3deb071b498b6ba8c01e7ed3e2083
Aarush-wadhia <aarushwadhia@gmail.com>
2022-06-16 12:28:39 +0530

    feat(Jotai): Implemented jotai library

---
+++ with-jotai/atoms.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+const counterAtom = atom<number>(0);
+
+export default counterAtom;
---
+++ with-jotai/popup.tsx
@@ -0,0 +1,13 @@
+import {Provider} from "jotai";
+
+import CounterView from "~counter";
+
+function IndexPopup() {
+  return (
+    <Provider>
+      <CounterView />
+    </Provider>
+  )
+}
+
+export default IndexPopup
