0a59060b48fd034e6a371341d0c6f72a9c145762
Yash Wadhia <45955091+Aarush-wadhia@users.noreply.github.com>
2022-06-16 22:36:58 +0530

    feat: Implemented jotai library (#5)
    * feat(Jotai): Implemented jotai library
    
    * fix: remove atom.ts file and merge the code in counter.tsx file
    
    * doc(Package.json): Added description in the package.json file

---
+++ with-jotai/counter.tsx
@@ -0,0 +1,24 @@
+import { atom, useAtom } from "jotai"
+
+const counterAtom = atom<number>(0);
+
+const CounterView = () => {
+    const [counter, setCounter] = useAtom(counterAtom);
+
+    const onIncrement = (): void => {
+        setCounter((value: number): number => value + 1);
+    }
+
+    const onDecrement = (): void => {
+        setCounter((value: number): number => value - 1);
+    }
+  return (
+    <div>
+      <div>Current count: {counter}</div>
+      <button onClick={onIncrement}>Increment counter</button>
+      <button onClick={onDecrement}>Decrement counter</button>
+    </div>
+  )
+}
+
+export default CounterView;
