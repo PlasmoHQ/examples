f9dcaa3756f13bca4ca5b1f55a1b92792012426c
Louis <6723574+louisgv@users.noreply.github.com>
2022-06-17 02:15:07 -0400

    Bump dependencies

--- with-react-router/routes/index.tsx
+++ with-react-router/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 
-import { About } from "~pages/about"
-import { Home } from "~pages/home"
+import { About } from "./about"
+import { Home } from "./home"
 
 export const Routing = () => (
   <Routes>
