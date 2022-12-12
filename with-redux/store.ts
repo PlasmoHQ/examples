25761342124638c9bfcca71cc1e1c207a550195a
G4den <gade1201@gmail.com>
2022-12-13 00:46:37 +0100

    feat: Redux hooks with defined types using workaround
    A workaround is made to infer types from a mock store.
    
    The hooks with defined types is recommended by redux toolkit and is the standard when using redux with typescript
    
    https://react-redux.js.org/using-react-redux/usage-with-typescript#define-typed-hooks

--- with-redux/store.ts
+++ with-redux/store.ts
@@ -1,3 +1,5 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+
 import {
   FLUSH,
   PAUSE,
@@ -10,44 +12,63 @@ import {
   persistStore
 } from "@plasmohq/redux-persist"
 import { Storage } from "@plasmohq/storage"
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import { localStorage } from "redux-persist-webextension-storage"
 
 import counterSlice from "~counter-slice"
 
-const rootReducer = counterSlice
+// Here you can add all your reducers
+const combinedReducers = combineReducers({
+    counter: counterSlice
+});
 
 const persistConfig = {
-  key: "root",
-  version: 1,
-  storage: localStorage
+    key: "root",
+    version: 1,
+    storage: localStorage
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+// TODO: Fix persistReducer so it doesn't break the types
+const persistedReducer = persistReducer(persistConfig, combinedReducers)
 
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          FLUSH,
-          REHYDRATE,
-          PAUSE,
-          PERSIST,
-          PURGE,
-          REGISTER,
-          RESYNC
-        ]
-      }
-    })
+// Until persistReducer is fixed, we need to use this mock store to get the types
+const mockStore = configureStore({
+    reducer: combinedReducers
 })
+
+// @ts-ignore
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                    RESYNC
+                ]
+            }
+        })
+}) as typeof mockStore;
+
 export const persistor = persistStore(store)
 
 // This is what makes Redux sync properly with multiple pages
 // Open your extension's options page and popup to see it in action
 new Storage().watch({
-  [`persist:${persistConfig.key}`]: () => {
-    persistor.resync()
-  }
+    [`persist:${persistConfig.key}`]: () => {
+        persistor.resync()
+    }
 })
+
+// Get the types from the mock store
+export type RootState = ReturnType<typeof mockStore.getState>;
+export type AppDispatch = typeof mockStore.dispatch;
+
+// Export the hooks with the types from the mock store
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
