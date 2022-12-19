07bc602024dcfde9c366e40a687522bfaf1231d0
Louis <6723574+louisgv@users.noreply.github.com>
2022-12-19 17:49:17 -0400

    fix up with local id

--- with-antd/theme.tsx
+++ with-antd/theme.tsx
@@ -1,6 +1,5 @@
 import { ConfigProvider } from "antd"
 import type { ReactNode } from "react"
-import { StyleProvider } from "@ant-design/cssinjs";
 
 export const ThemeProvider = ({ children = null as ReactNode }) => (
   <ConfigProvider
@@ -9,8 +8,6 @@ export const ThemeProvider = ({ children = null as ReactNode }) => (
         colorPrimary: "#a1701d"
       }
     }}>
-      <StyleProvider container={document.querySelector("plasmo-csui").shadowRoot}>
-        {children}
-      </StyleProvider>
+    {children}
   </ConfigProvider>
 )
