de42aa8fe74e2b025fa582112d59795c7c04ba4e
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-12-27 12:42:30 +0800

    feat: with-tabs (#24)

---
+++ with-tabs/tabs/delta-flyer.tsx
@@ -0,0 +1,16 @@
+function DeltaFlyerPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <h2>Delta Flyer Tab</h2>
+ 
+      <p>This tab is only available on the Delta Flyer page.</p>
+    </div>
+  )
+}
+ 
+export default DeltaFlyerPage
