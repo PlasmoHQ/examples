9f9d97284261337245311679aa6f307f6942566b
Louis <6723574+louisgv@users.noreply.github.com>
2023-01-06 02:24:03 -0400

    use class instead of media

--- with-tailwindcss/tailwind.config.js
+++ with-tailwindcss/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{tsx,html}"],
-  darkMode: "media",
+  darkMode: "class",
   plugins: [require("windy-radix-palette")]
 }
