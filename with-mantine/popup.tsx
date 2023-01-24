29109a52cd8e6f9e8b3b9df6911608f7eeca9ae2
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-01-24 19:16:14 +0800

    feat: with mantine (#28)

---
+++ with-mantine/popup.tsx
@@ -0,0 +1,27 @@
+import { Stack, Button, Anchor, Text, Input } from '@mantine/core'
+import { useState } from "react"
+import { ThemeProvider } from '~theme'
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <ThemeProvider>
+      <Stack miw={240} p="lg">
+        <Text fw="bold" size="xl">
+          Welcome to your{" "}
+          <Anchor href="https://www.plasmo.com" target="_blank">
+            Plasmo
+          </Anchor>{" "}
+          Extension!
+        </Text>
+        <Input onChange={(e) => setData(e.target.value)} value={data} />
+        <Button component="a" href="https://docs.plasmo.com" target="_blank">
+          View Docs
+        </Button>
+      </Stack>
+    </ThemeProvider>
+  )
+}
+
+export default IndexPopup
