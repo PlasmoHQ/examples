c3412b3611b1569af7e6a8c5bde717838673d706
gemsvidø <gemsvido@gmail.com>
2023-01-27 22:11:44 +0000

    Add with-dnr

---
+++ with-dnr/popup.tsx
@@ -0,0 +1,11 @@
+import someCoolImage from "data-base64:~assets/icon512.png"
+
+function IndexPopup() {
+  return (
+    <div>
+      <img src={someCoolImage} alt="Some pretty cool image" />
+    </div>
+  )
+}
+
+export default IndexPopup
