491699a81457122b4a5fba8e1a93c483ef51b48a
Louis <6723574+louisgv@users.noreply.github.com>
2023-01-28 22:41:01 -0400

    chore: clean up env example

--- with-env/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PLASMO_PUBLIC_SHIP_NAME?: string
-    PLASMO_PUBLIC_SHIELD_FREQUENCY?: string
-
-    PLASMO_PUBLIC_SITE_URL?: string
-  }
-}
