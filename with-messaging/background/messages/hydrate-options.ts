82b66f14892fd467e82dbe4ac592e85049a010bf
Louis <6723574+louisgv@users.noreply.github.com>
2023-01-30 02:19:30 -0400

    fix messaging API

--- with-messaging/background/messages/hydrate-options.ts
+++ with-messaging/background/messages/hydrate-options.ts
@@ -1,6 +1,8 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-export const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   console.log(req)
   res.send("Show me something cool man")
 }
+
+export default handler
