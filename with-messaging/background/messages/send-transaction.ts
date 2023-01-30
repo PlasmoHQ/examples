e49d6a16719fcca6962c2ff1dcb857390e48b21a
Louis <6723574+louisgv@users.noreply.github.com>
2023-01-30 02:08:51 -0400

    fix: update tsconfig

--- with-messaging/background/messages/send-transaction.ts
+++ with-messaging/background/messages/send-transaction.ts
@@ -1,9 +1,11 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-export const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   console.log(req)
 
   res.send({
     message: "Transaction Send completed"
   })
 }
+
+export default handler
--- with-messaging/background/ports/mail.ts
+++ with-messaging/background/ports/mail.ts
@@ -1,9 +1,11 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-export const handler: PlasmoMessaging.PortHandler = async (req, res) => {
+const handler: PlasmoMessaging.PortHandler = async (req, res) => {
   console.log(req)
 
   res.send({
     message: "Hello from port handler"
   })
 }
+
+export default handler
