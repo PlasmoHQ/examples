19c7c4aa55fba85db1627c526870dc0c9c5c489d
Louis <6723574+louisgv@users.noreply.github.com>
2023-02-09 15:18:46 -0400

    Update main-world example with run_at

--- with-main-world/contents/plasmo-main.ts
+++ with-main-world/contents/plasmo-main.ts
@@ -2,7 +2,8 @@ import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.plasmo.com/*"],
-  world: "MAIN"
+  world: "MAIN",
+  run_at: "document_start"
 }
 
 window.mainMessage = "Sometimes a feeling is all we humans have to go on."
