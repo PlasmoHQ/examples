c4d4a6fbc2813a3a9061b3fe75df0c2f2fa87c7e
Louis <6723574+louisgv@users.noreply.github.com>
2023-03-03 18:58:43 -0400

    fix(with-selector): cs matches

--- with-selector/content.tsx
+++ with-selector/content.tsx
@@ -3,7 +3,7 @@ import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
 import { querySelector } from "@plasmohq/selector"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://plasmo.com/*"]
+  matches: ["https://www.plasmo.com/*"]
 }
 
 export const getInlineAnchor: PlasmoGetInlineAnchor = () => {
