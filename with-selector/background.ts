632c9dc91fc5c8bc5d08458eaa39fe82df9f930c
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-03-15 22:12:48 -0600

    chore: update example again

--- with-selector/background.ts
+++ with-selector/background.ts
@@ -1,7 +1,5 @@
 import { init } from "@plasmohq/selector/background"
 
-console.log(process.env.PLASMO_PUBLIC_ITERO_SELECTOR_MONITOR_ID)
-
 init({
   monitorId: process.env.PLASMO_PUBLIC_ITERO_SELECTOR_MONITOR_ID
 })
