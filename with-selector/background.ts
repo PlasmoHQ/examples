ea8de89b77aa9cfef16f1532ff34d8f3f0d96597
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-03-15 22:10:09 -0600

    fix(with-selector): alignining env

--- with-selector/background.ts
+++ with-selector/background.ts
@@ -1,5 +1,7 @@
 import { init } from "@plasmohq/selector/background"
 
+console.log(process.env.PLASMO_PUBLIC_ITERO_SELECTOR_MONITOR_ID)
+
 init({
-  monitorId: process.env.PLASMO_PUBLIC_ITERO_MONITOR_ID
+  monitorId: process.env.PLASMO_PUBLIC_ITERO_SELECTOR_MONITOR_ID
 })
