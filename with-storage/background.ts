aa6733f4e06f58aef1bba0eef0797d815137c946
Louis <6723574+louisgv@users.noreply.github.com>
2023-04-09 17:35:56 -0400

    Add base storage example

--- with-storage/background.ts
+++ with-storage/background.ts
@@ -1,3 +1,4 @@
+import { Storage } from "@plasmohq/storage"
 import { SecureStorage } from "@plasmohq/storage/secure"
 import assert from "assert"
 
@@ -5,7 +6,7 @@ const PASSWORD = "password"
 const TEST_KEY = "ship"
 const TEST_DATA = "1701"
 
-const main = async () => {
+async function testSecureStorage() {
   const storage = new SecureStorage({ area: "local" })
 
   await storage.setPassword(PASSWORD)
@@ -19,6 +20,36 @@ const main = async () => {
   console.log(await storage.getAll())
 
   await storage.clear()
+}
+
+async function testBaseStorage() {
+  const storage = new Storage()
+  await storage.clear()
+
+  storage.watch({
+    hello: (c) => {
+      console.log("hello", c.newValue)
+    },
+    "serial-number": (c) => {
+      console.log("serial-number", c.newValue)
+    },
+    make: (c) => {
+      console.log("make", c.newValue)
+    }
+  })
+
+  await storage.set("hello", 1)
+  await storage.set("serial-number", 1701)
+  await storage.set("make", "plasmo-corp")
+
+  await storage.set("hello", 2)
+  await storage.set("serial-number", 8451)
+  await storage.set("make", "PlasmoHQ")
+}
+
+const main = async () => {
+  await testSecureStorage()
+  await testBaseStorage()
 }
 
 main()
