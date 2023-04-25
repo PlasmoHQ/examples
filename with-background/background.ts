007c7256b78650aeb3cd0ddbfdef6b057e170e8a
Louis <6723574+louisgv@users.noreply.github.com>
2023-04-25 16:10:30 -0400

    add background with action api

--- with-background/background.ts
+++ with-background/background.ts
@@ -3,4 +3,7 @@ import { generateMnemonic } from "bip39"
 console.log(
   "Live now; make now always the most precious time. Now will never come again."
 )
-console.log(generateMnemonic())
+
+chrome.action.onClicked.addListener(() => {
+  console.log(generateMnemonic())
+})
