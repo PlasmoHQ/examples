e51f9d05da839a2c5e115de3bb515ef9f018f239
Mark Perry <mp@markperry.co.uk>
2023-06-25 14:12:04 +0100

    feat: add pub/sub to with-messaging example

--- with-messaging/background/index.ts
+++ with-messaging/background/index.ts
@@ -1,4 +1,6 @@
 import "@plasmohq/messaging/background"
 
-export const life = 47
-console.log(`WELCOME - ${life}`)
+import { startHub } from "@plasmohq/messaging/pub-sub"
+
+console.log(`BGSW - Starting Hub`)
+startHub()
