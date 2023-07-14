8d9013129df3df4877c55c5fa45ab5e6d6ae2425
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-07-13 20:00:58 -0700

    chore: bump dependencies

--- with-supabase/core/supabase.ts
+++ with-supabase/core/supabase.ts
@@ -1,6 +1,30 @@
 import { createClient } from "@supabase/supabase-js"
 
+import {
+  Storage
+} from "@plasmohq/storage"
+
+const storage = new Storage()
+
 export const supabase = createClient(
   process.env.PLASMO_PUBLIC_SUPABASE_URL,
-  process.env.PLASMO_PUBLIC_SUPABASE_KEY
+  process.env.PLASMO_PUBLIC_SUPABASE_KEY,
+  {
+    auth: {
+      storage: {
+        getItem(key) {
+          return storage.get(key)
+        },
+        setItem(key, data) {
+          return storage.set(key, data)
+        },
+        removeItem(key) {
+          return storage.remove(key)
+        }
+      },
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true,
+    }
+  }
 )
