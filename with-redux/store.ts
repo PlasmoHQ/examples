ead8149eff0e4c276fcd4f07bcc603d9d57409c4
Zweihänder <zweidev@zweihander.me>
2023-09-21 14:49:29 -0400

    fix: with-redux import typed hook as type
    Change made in response to the following TS error:
    'TypedUseSelectorHook' is a type and must be imported using a type-only
    import when 'verbatimModuleSyntax' is enabled.
    
    Error shows up when creating a new repository using `pnpm create plasmo
    --with-redux`. Fix doesn't affect useAppSelector hook in Counter.tsx.

--- with-redux/store.ts
+++ with-redux/store.ts
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import type { TypedUseSelectorHook } from "react-redux"
 import { syncStorage } from "redux-persist-webextension-storage"
 
 import {
