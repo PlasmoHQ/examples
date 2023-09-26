57b2d87cec941c22e0e8d2ed2b38f78ef722f92f
James <james@arnott.tech>
2023-09-26 13:59:36 +0100

    docs: added example env
    chore: updating manifest + contributors
    
    feat: using google analytics measurement protocol

--- with-google-analytics/popup.tsx
+++ with-google-analytics/popup.tsx
@@ -1,37 +1,55 @@
-import "https://www.googletagmanager.com/gtag/js?id=$PLASMO_PUBLIC_GTAG_ID"
-
 import { useEffect, useState } from "react"
+import { AnalyticsEvent } from "~utils";
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   useEffect(() => {
-    window.dataLayer = window.dataLayer || []
-    window.gtag = function gtag() {
-      window.dataLayer.push(arguments) // eslint-disable-line
-    }
-    window.gtag("js", new Date())
-    window.gtag("config", process.env.PLASMO_PUBLIC_GTAG_ID, {
-      page_path: "/popup",
-      debug_mode: true
-    })
-
-    window.gtag("event", "login", {
-      method: "TEST"
-    })
-  }, [])
+    AnalyticsEvent([
+      {
+        name: 'page_view',
+        params: {
+          page_path: '/popup',
+          debug_mode: true
+        }
+      }
+    ]);
+    setTimeout(() => {
+      AnalyticsEvent([
+        {
+          name: 'page_view',
+          params: {
+            page_path: '/popup',
+            debug_mode: true,
+            duration: 5,
+          }
+        }
+      ]);
+    }, 5000)
+  }, []);
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        padding: 16,
+        minWidth: "250px",
       }}>
       <h1>
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
       <input onChange={(e) => setData(e.target.value)} value={data} />
+      <button onClick={() => AnalyticsEvent([
+        {
+          name: 'button_click',
+          params: {
+            button_name: 'click_me',
+            debug_mode: true,
+            data: data,
+          }
+        }
+      ])}>Click Me</button>
     </div>
   )
 }
