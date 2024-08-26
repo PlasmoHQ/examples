3f2ab2cfda70c94ba26c0c4aec7331aae29ddec1
Theodore Brockman <iam@theo.lol>
2024-08-25 20:33:46 -0400

    Add 'with-browser-specific-overrides' example.

---
+++ with-browser-specific-overrides/background.ts
@@ -0,0 +1,17 @@
+import { generateMnemonic } from "bip39"
+
+console.log(
+  "Live now; make now always the most precious time. Now will never come again."
+)
+
+chrome.action.onClicked.addListener(() => {
+  console.log(`action clicked: ${generateMnemonic()}`)
+})
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "test") {
+    console.log(`test command: ${generateMnemonic()}`)
+  } else if (command === "test-chrome") {
+    console.log(`chrome specific command: ${generateMnemonic()}`)
+  }
+})
