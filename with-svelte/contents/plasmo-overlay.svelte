647818cdae25031a30feaaf34fb30073a64c1f5a
Joaquin Esteban <dev@joacoesteban.com>
2024-08-26 15:36:48 +0200

    Add with-svelte-injected-style example (#63)
    * Add with-svelte-injected-style example
    
    * Remove with-svelte-injected-style
    
    * Implement injected styles

--- with-svelte/contents/plasmo-overlay.svelte
+++ with-svelte/contents/plasmo-overlay.svelte
@@ -14,3 +14,13 @@
 </script>
 
 <span class="hw-top">Every living thing wants to survive</span>
+
+<style>
+  .hw-top {
+    background: red;
+    color: white;
+    font-family: "Fascinate";
+    width: 200px;
+    border: 8px solid aqua;
+  }
+</style>
