07ea667e04aef259f004817fd3923e697d3e4abe
Theodore Brockman <iam@theo.lol>
2024-09-07 13:49:17 -0400

    Fix with-jest tests.

--- with-jest/jest.config.mjs
+++ with-jest/jest.config.mjs
@@ -1,25 +1,17 @@
-import { createRequire } from "module"
-import { pathsToModuleNameMapper } from "ts-jest"
-
-const require = createRequire(import.meta.url)
-const tsconfig = require("./tsconfig.json")
-
 /**
  * @type {import('@jest/types').Config.InitialOptions}
  */
 
 const config = {
   setupFiles: ["jest-webextension-mock"],
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
   testRegex: ["^.+\\.test.tsx?$"],
-  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
-    prefix: "<rootDir>/"
-  }),
+  moduleNameMapper: {
+    "~(.*)": "<rootDir>/$1",
+  },
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.ts?$": ["ts-jest", { isolatedModules: true, useESM: true }],
-    "^.+\\.tsx?$": ["ts-jest", { useESM: true, tsconfig: { jsx: "react-jsx" } }]
-  }
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
 }
 
 export default config
