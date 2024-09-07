5b76e8e3d12d281fb32134521f03f2bffecdcd0f
Theodore Brockman <iam@theo.lol>
2024-09-07 14:40:20 -0400

    Undo moduleNameMapper changes.

--- with-jest/jest.config.mjs
+++ with-jest/jest.config.mjs
@@ -1,13 +1,20 @@
+import { createRequire } from "module"
+import { pathsToModuleNameMapper } from "ts-jest"
+
+const require = createRequire(import.meta.url)
+const tsconfig = require("./tsconfig.json")
+
 /**
  * @type {import('@jest/types').Config.InitialOptions}
  */
 
 const config = {
   setupFiles: ["jest-webextension-mock"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   testRegex: ["^.+\\.test.tsx?$"],
-  moduleNameMapper: {
-    "~(.*)": "<rootDir>/$1",
-  },
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: "<rootDir>/"
+  }),
   testEnvironment: "jsdom",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
