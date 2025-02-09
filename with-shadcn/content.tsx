63131f588d8158083e0d7a2dfca638fde7163579
BalutaAlex <73162322+BalutaAlex@users.noreply.github.com>
2025-02-09 10:32:10 +0200

    feat: add with-shadcn example (#73)

---
+++ with-shadcn/content.tsx
@@ -0,0 +1,48 @@
+import cssText from "data-text:~globals.css"
+import type { PlasmoCSConfig } from "plasmo"
+
+import { LoginForm } from "~components/login-form"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"]
+}
+
+const styleElement = document.createElement("style")
+
+/**
+ * Generates a style element with adjusted CSS to work correctly within a Shadow DOM.
+ *
+ * Tailwind CSS relies on `rem` units, which are based on the root font size (typically defined on the <html>
+ * or <body> element). However, in a Shadow DOM (as used by Plasmo), there is no native root element, so the
+ * rem values would reference the actual page's root font size—often leading to sizing inconsistencies.
+ *
+ * To address this, we:
+ * 1. Replace the `:root` selector with `:host(plasmo-csui)` to properly scope the styles within the Shadow DOM.
+ * 2. Convert all `rem` units to pixel values using a fixed base font size, ensuring consistent styling
+ *    regardless of the host page's font size.
+ */
+export const getStyle = (): HTMLStyleElement => {
+  const baseFontSize = 16
+
+  let updatedCssText = cssText.replaceAll(":root", ":host(plasmo-csui)")
+  const remRegex = /([\d.]+)rem/g
+  updatedCssText = updatedCssText.replace(remRegex, (match, remValue) => {
+    const pixelsValue = parseFloat(remValue) * baseFontSize
+
+    return `${pixelsValue}px`
+  })
+
+  styleElement.textContent = updatedCssText
+
+  return styleElement
+}
+
+const CSUIExample = () => {
+  return (
+    <div className="flex w-[400px] flex-col border-2 bg-yellow-50">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default CSUIExample
---
+++ with-shadcn/popup.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/login-form"
+
+import "globals.css"
+
+const IndexPopup = () => {
+  return (
+    <div className="flex h-[25rem] w-[25rem] flex-col">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default IndexPopup
