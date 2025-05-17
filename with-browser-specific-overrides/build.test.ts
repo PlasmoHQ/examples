9368302113b11b2978f334383bdec85fb6d86c7c
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2025-05-17 13:56:23 -0700

    chore: fix browser test

--- with-browser-specific-overrides/build.test.ts
+++ with-browser-specific-overrides/build.test.ts
@@ -1,28 +1,29 @@
-import { assert } from 'chai';
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { exec } from "child_process"
+import { promisify } from "util"
+import { assert } from "chai"
 
-describe('with-browser-specific-overrides', () => {
+describe("with-browser-specific-overrides", () => {
+  const pexec = promisify(exec)
 
-    let pexec = promisify(exec)
+  it("contains no overrides or specific settings if not targeted", async () => {
+    await pexec("pnpm build --target=firefox-mv3")
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const manifest = require("./build/firefox-mv3-prod/manifest.json")
+    const commands = manifest.commands
+    assert.isDefined(commands)
+    assert.isDefined(commands["test"])
+    assert.isUndefined(commands["test-chrome"])
+    assert.isUndefined(manifest.overrides)
+  }).timeout(10000)
 
-    it('contains no overrides or specific settings if not targeted', async () => {
-        await pexec("pnpm build --target=firefox-mv3")
-        const manifest = require('./build/firefox-mv3-prod/manifest.json')
-        const commands = manifest.commands
-        assert.isDefined(commands)
-        assert.isDefined(commands['test'])
-        assert.isUndefined(commands['test-chrome'])
-        assert.isUndefined(manifest.overrides)
-    }).timeout(10000)
-
-    it('contains browser specific settings if targeted', async () => {
-        await pexec("pnpm build --target=chrome-mv3")
-        const manifest = require('./build/chrome-mv3-prod/manifest.json')
-        const commands = manifest.commands
-        assert.isDefined(commands)
-        assert.isDefined(commands['test'])
-        assert.isDefined(commands['test-chrome'])
-        assert.isUndefined(manifest.overrides)
-    }).timeout(10000)
+  it("contains browser specific settings if targeted", async () => {
+    await pexec("pnpm build --target=chrome-mv3")
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const manifest = require("./build/chrome-mv3-prod/manifest.json")
+    const commands = manifest.commands
+    assert.isDefined(commands)
+    assert.isDefined(commands["test"])
+    assert.isDefined(commands["test-chrome"])
+    assert.isUndefined(manifest.overrides)
+  }).timeout(10000)
 })
